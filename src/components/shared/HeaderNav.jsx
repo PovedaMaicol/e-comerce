ba7b7a6337cdad304b03f477c470bcb04f51a511
Maicol Salazar <stivsal12@gmail.com>
2024-04-16 21:03:10 -0500

    state optimization

--- src/components/shared/HeaderNav.jsx
+++ src/components/shared/HeaderNav.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './styles/headerNav.css';
-const body = document.querySelector('body');
-const icono = document.querySelector('.bx-toggle-left');
 
-const handleDark = () => {
-  body.classList.toggle('dark');
-  icono.classList.toggle('bx-toggle-right');
+// const icono = document.querySelector('.bx-toggle-left');
+
+// const handleDark = () => {
+//   
+//   icono.classList.toggle('bx-toggle-right');
   
-}
+// }
 
 
 const HeaderNav = () => {
+const body = document.querySelector('body');
+const [ isActived, setIsActived ] = useState(false);
+const handleDark = () => {
+  setIsActived(!isActived)
+  body.classList.toggle('dark');
+};
+
   return (
     <div className='headerNav'>
         <h1><Link to='/'>e-commerce</Link></h1>
@@ -20,7 +27,7 @@ const HeaderNav = () => {
             <li><Link to='/login'><i className='bx bx-user'></i></Link></li>
             <li><Link to='/purchases'><i className='bx bx-box'></i></Link></li>
             <li><Link to='/cart'><i className='bx bx-cart'></i></Link></li>
-            <li><i className='bx bx-toggle-left' onClick={handleDark}></i></li>
+            <li><i onClick={handleDark} className={ isActived ? 'bx bx-toggle-left' : 'bx bx-toggle-right'}></i></li>
           </ul>
         </nav>
     </div>
